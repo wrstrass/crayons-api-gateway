a1d2973fe48ea8749a86c4ae14777ab262c44177
Anton <wrstrass@gmail.com>
2023-04-16 03:08:11 +0300

    Gateway config

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from utils import async_post
 
-app = FastAPI()
+
+PREFIX = "/api/v1"
+
+MICROSERVICES = {
+    "auth": "http://auth:8001/api/v1/auth",
+}
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = FastAPI(
+    docs_url=f"{PREFIX}/docs/",
+    redoc_url=f"{PREFIX}/redoc/",
+    openapi_url=f"{PREFIX}/openapi.json",
+)
+
+
+@app.get(f"{PREFIX}/register/")
+async def register():
+    response = await async_post(f"{MICROSERVICES['auth']}/register/")
+    return response
---
+++ utils.py
@@ -0,0 +1,11 @@
+import aiohttp
+
+async def async_get(url, **kwargs) -> aiohttp.ClientResponse:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, **kwargs) as response:
+            return await response.json()
+
+async def async_post(url, data = None, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=data, **kwargs) as response:
+            return await response.json()
