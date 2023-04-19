8a762e04c7b2253a0ac145f7d8fb954aa8e924ee
Anton <wrstrass@gmail.com>
2023-04-19 22:19:12 +0300

    Change login to username

--- auth/schemas.py
+++ auth/schemas.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel, constr
 
 
 class AuthSchema(BaseModel):
-    login: constr(min_length=5, strict=True)
+    username: constr(min_length=5, strict=True)
     password: constr(min_length=8, strict=True)
