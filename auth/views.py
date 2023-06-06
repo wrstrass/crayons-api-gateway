5bbc64f9460475bbda0a1c84b19dcb4fe260ccbd
Anton <wrstrass@gmail.com>
2023-06-07 01:05:52 +0300

    Bugfix register

--- auth/views.py
+++ auth/views.py
@@ -20,7 +20,8 @@ router = APIRouter(
 })
 async def register(auth: AuthSchema):
     res = await async_post(f"{auth_url}/register", auth.dict())
-    res = await async_post(f"{auth_url}/login", auth.dict())
+    if res.status == 200:
+        res = await async_post(f"{auth_url}/login", auth.dict())
     return res.to_response()
 
 
