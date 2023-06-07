4557a64bfb11d12243a360c544681b33a38cf493
Anton <wrstrass@gmail.com>
2023-06-07 04:00:30 +0300

    Add get user by id

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from user.schemas import UserSearchSchema, UserSchema
-from utils import async_post
+from utils import async_get, async_post
 from const import MICROSERVICES
 
 
@@ -16,4 +16,9 @@ router = APIRouter(
 @router.post("/search")
 async def search(search_schema: UserSearchSchema) -> list[UserSchema]:
     res = await async_post(f"{user_url}/search", search_schema.dict())
+    return res.to_response()
+
+@router.get("/{user_id}")
+async def get_by_id(user_id: int) -> str:
+    res = await async_get(f"{user_url}/{user_id}")
     return res.to_response()
